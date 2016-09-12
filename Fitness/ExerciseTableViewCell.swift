5ffcf19421c0532892d3212a9bcc4ae781f59080
Ryan Ploetz <ryan@venuenext.com>
2016-09-11 22:18:44 -0500

    Setting up some basic navigation

---
+++ Fitness/ExerciseTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  ExerciseTableViewCell.swift
+//  Fitness
+//
+//  Created by Ryan Ploetz on 9/11/16.
+//  Copyright © 2016 Baygull Studios LLC. All rights reserved.
+//
+
+import UIKit
+
+class ExerciseTableViewCell: UITableViewCell {
+
+    // MARK: Properties
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    // MARK: Functions
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
